ddcb6d8942b29701887adfb354cf21472ec2e68d
yuvraj1920 <yuvrajsinghdec19@gmail.com>
2025-07-16 22:11:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import os
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
